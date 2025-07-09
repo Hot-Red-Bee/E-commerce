c985e021187f2379cf483c35f7a72890298e86a5
TheEliteCoder001 <sethvybzshows1@gmailcom>
2025-07-09 05:43:29 +0100

    Fixed routes spelling errors

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -4,22 +4,22 @@ import connectDB from "./config/db.js";
 const dotenv = require("dotenv"); //Changed import to require, syntax conflit resolved
 
 // Import routes
-import cartRoute from "./routes/cartRoutes.js"; //Fixed cartRoutes to cartRoute
+import cartRoute from "./routes/cartRoutes.js"; //Fixed cartRoutes spelling to cartRoute
 app.use("/api/cart", cartRoute);
 
-import orderRoutes from "./routes/orderRoutes.js";
-app.use("/api/orders", orderRoutes);
+import orderRoute from "./routes/orderRoutes.js";
+app.use("/api/orders", orderRoute);
 
-import productRoutes from "./routes/productRoutes.js";
+import productRoute from "./routes/productRoute.js";
 
-import userRoutes from "./routes/userRoute.js";
-app.use("/api/auth", userRoutes);
+import userRoute from "./routes/userRoute.js";
+app.use("/api/auth", userRoute);
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use("/api/products", productRoutes);
+app.use("/api/products", productRoute);
 
 
 const app = express();
